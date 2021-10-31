177c1fa92e8b4c6107470f356f6a7b37ea5d3c61
Marc Reichel <mail@marcreichel.de>
2021-10-31 17:27:23 +0100

    ♻️ Improve model maintainability
    The endpoint gets generated automatically (by default)

--- src/Models/Subtype.php
+++ src/Models/Subtype.php
@@ -5,15 +5,10 @@ namespace Slaty\LaravelPokemontcg\Models;
 class Subtype extends Model
 {
     /**
-     * @var string
-     */
-    public const ENDPOINT = '/subtypes';
-
-    /**
      * @return mixed
      */
     public function all(): mixed
     {
-        return $this->resolveResponse($this->client->get(self::ENDPOINT), self::ENDPOINT);
+        return $this->resolveResponse($this->client->get($this->getEndpoint()), $this->getEndpoint());
     }
 }
--- src/Models/Supertype.php
+++ src/Models/Supertype.php
@@ -5,15 +5,10 @@ namespace Slaty\LaravelPokemontcg\Models;
 class Supertype extends Model
 {
     /**
-     * @var string
-     */
-    public const ENDPOINT = '/supertypes';
-
-    /**
      * @return mixed
      */
     public function all(): mixed
     {
-        return $this->resolveResponse($this->client->get(self::ENDPOINT), self::ENDPOINT);
+        return $this->resolveResponse($this->client->get($this->getEndpoint()), $this->getEndpoint());
     }
 }
