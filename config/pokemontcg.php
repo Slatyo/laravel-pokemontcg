212b18ad82065219e7af3b429aa4545372761741
Daniel Henze <daniel.henze@outlook.com>
2021-10-31 21:32:49 +0100

    :memo: Update readme

--- config/pokemontcg.php
+++ config/pokemontcg.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * You can place your custom package configuration in here.
+ * Default configuration to run the pokemontcg.io API
  */
 return [
     'url' => env('POKEMONTCG_API_URL', 'https://api.pokemontcg.io/v2'),
