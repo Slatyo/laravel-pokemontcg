169e640bec64cce899ef17e358532ab7bb9b4a9c
Daniel Henze <daniel.henze@outlook.com>
2021-11-01 03:20:05 +0100

    Fix publish

--- src/LaravelPokemontcgServiceProvider.php
+++ src/LaravelPokemontcgServiceProvider.php
@@ -50,7 +50,7 @@ class LaravelPokemontcgServiceProvider extends ServiceProvider
     public function register()
     {
         // Automatically apply the package configuration
-        $this->mergeConfigFrom(__DIR__.'/../config/pokemontcg.php', 'laravel-pokemontcg');
+        $this->mergeConfigFrom(__DIR__.'/../config/pokemontcg.php', 'pokemontcg');
 
         // Register the main class to use with the facade
         $this->app->singleton('pokemontcg', function () {
